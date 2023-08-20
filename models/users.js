377e5feac678739619509b56810218789efb7f46
Jeremiah Joy Joseph <jeremiahjoyjoseph97@gmail.com>
2023-08-20 16:24:31 +0530

    feature: user model

--- models/users.js
+++ models/users.js
@@ -1,6 +1,6 @@
-const moment = require("moment");
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
+const validator = require("validator");
 
 const UserSchema = new mongoose.Schema(
   {
@@ -26,6 +26,7 @@ const UserSchema = new mongoose.Schema(
       trim: true,
       unique: true,
       required: [true, "Please enter email."],
+      validate: [validator.isEmail, "Please enter valid email address."],
     },
     phNo: {
       type: String,
@@ -38,24 +39,38 @@ const UserSchema = new mongoose.Schema(
       trim: true,
       required: [true, "Please provide unique username."],
     },
+    password: {
+      type: String,
+      required: [true, "Please enter password."],
+      minlength: [8, "Your password must be at least 8 characters long."],
+      select: false, //Hidden for normal responses.
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
     role: {
       type: String,
       trim: true,
       required: [true, "Please provide role of user."],
+      default: "guest",
       enum: {
         values: [
-          "Admin",
-          "Worship Leader",
-          "Worship Team",
-          "Media Team",
-          "Sound Team",
+          "admin",
+          "worship-leader",
+          "worship-team",
+          "media-team",
+          "sound-team",
+          "guest",
         ],
-        message: "Please enter valid role",
+        message: "Please select valid role.",
       },
-      lastLogInDate: {
-        type: Date,
-        select: false,
-      },
+    },
+    lastLogInDate: {
+      type: Date,
+      select: false,
     },
     slug: String,
   },
@@ -63,5 +78,12 @@ const UserSchema = new mongoose.Schema(
 );
 
 UserSchema.plugin(uniqueValidator, { message: "is already taken." });
+
+//Creating song slug before saving
+SongSchema.pre("save", function (next) {
+  //Creating slug before saving to DB
+  this.slug = slugify(this.title, { lower: true });
+  next();
+});
 
 module.exports = mongoose.model("Users", UserSchema);
