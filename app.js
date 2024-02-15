7e6c990c2ba1f40277e9fde7f9a7e7af9a0e4af1
Jeremiah Joy Joseph <jeremiahjoyjoseph97@gmail.com>
2024-02-15 23:30:13 +0530

    damn everything is working now after some re-arrangement

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
+
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
-const connectDatabase = require("./config/database");
-const errorMiddleware = require("./middleware/errors");
-const catchAsyncErrors = require("./middleware/catchAsyncErrors");
-const ErrorHandler = require("./util/errorHandler");
 const fileUpload = require("express-fileupload");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+
+const connectDatabase = require("./config/database");
+const errorMiddleware = require("./middleware/errors");
+const ErrorHandler = require("./util/errorHandler");
 const { NOT_FOUND } = require("./util/httpStatusCodes");
 
 //Setting up config.env file variables
@@ -29,10 +31,27 @@ app.use(express.json());
 //Set cookie parser
 app.use(cookieParser());
 
+// Handle file uploads
+app.use(fileUpload());
+
+//Use rate limit NOT WORKING
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 24 hrs in milliseconds
+  limit: 100,
+  message: "You have exceeded the 100 requests in 10 mins! Who are you?",
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+app.use(limiter);
+
+//Setup security headers
+app.use(helmet());
+
 //Importing all routes
 const songs = require("./routes/songs");
 const auth = require("./routes/auth");
 const user = require("./routes/user");
+
 app.use("/api/v1", auth);
 app.use("/api/v1", songs);
 app.use("/api/v1", user);
@@ -44,22 +63,6 @@ app.all("*", (req, res, next) => {
 
 //Middleware to handle errors
 app.use(errorMiddleware);
-
-//Middleware to handle async errors
-app.use(catchAsyncErrors);
-
-// Handle file uploads
-app.use(fileUpload());
-
-//Use rate limit NOT WORKING
-const limiter = rateLimit({
-  windowMs: 24 * 60 * 60 * 1000, // 24 hrs in milliseconds
-  limit: 100,
-  message: "You have exceeded the 100 requests in 24 hrs limit!",
-  standardHeaders: true,
-  legacyHeaders: false,
-});
-app.use(limiter);
 
 const PORT = process.env.PORT;
 const server = app.listen(PORT, () => {
