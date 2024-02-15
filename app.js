8911ad5686871ee3fe8338cbeb4b6c917a3e389a
Jeremiah Joy Joseph <jeremiahjoyjoseph97@gmail.com>
2024-02-15 23:10:36 +0530

    fix: limit

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use(fileUpload());
 //Use rate limit NOT WORKING
 const limiter = rateLimit({
   windowMs: 24 * 60 * 60 * 1000, // 24 hrs in milliseconds
-  limit: 1,
+  limit: 100,
   message: "You have exceeded the 100 requests in 24 hrs limit!",
   standardHeaders: true,
   legacyHeaders: false,
